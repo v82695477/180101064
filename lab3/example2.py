81ab3b7e5876b61a7cefd5739f00c6112de97916
v82695477 <mustafabayulke@std.iyte.edu.tr>
2020-10-26 08:41:25 +0000

    lab3 example2

--- lab3/example2.py
+++ lab3/example2.py
@@ -2,10 +2,11 @@ x1 = input("number 1 : ")
 x2 = input("number 2 : ")
 x3 = input("number 3 : ")
 
-if x1 < x2 :
+if x1 <= x2 :
   min_num = x1
-
-if x3 < min_num:
+else:
+  min_num = x2
+if x3 <= min_num:
   min_num = x3
 
 print(min_num)
